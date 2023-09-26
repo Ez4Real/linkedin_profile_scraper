f8c5ecbf519c84fb18f08ebe905e563c06e83d70
Ez4Real <butilka05roma@gmail.com>
2023-09-26 23:09:09 +0300

    modify startup.sh

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
-from config import SECRET_KEY
-from exception_handlers import unauthorized_handler
+from .config import SECRET_KEY
+from .exception_handlers import unauthorized_handler
 
-from panel.router import panel_router
-from auth.router import auth_router
+from .panel.router import panel_router
+from .auth.router import auth_router
 
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 
 app.mount("/static/panel/", StaticFiles(directory="static/panel"), name="panel_static")
 app.mount("/static/auth/", StaticFiles(directory="static/auth"), name="auth_static")
